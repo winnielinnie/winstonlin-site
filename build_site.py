8b24e1bddeebbadd0126fac359f839036662c91f
Winston Lin <261500233+wnstn24@users.noreply.github.com>
2026-05-26 09:16:44 -0700

    Fix card text overflow

--- build_site.py
+++ build_site.py
@@ -14,7 +14,7 @@ POSTS_DIR = CONTENT_DIR / "posts"
 STATIC_DIR = ROOT / "static"
 DIST_DIR = ROOT / "dist"
 OUTPUT_DIR = ROOT / "docs"
-ASSET_VERSION = "20260526-polish"
+ASSET_VERSION = "20260526-overflow-fix"
 
 
 @dataclass
